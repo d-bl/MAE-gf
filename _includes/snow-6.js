7c85901fba1d93555861c87dd95211a0584ec17b
jo-pol <jo-pol@users.noreply.github.com>
2024-06-16 16:33:37 +0200

    snow in head/foot-side

--- _includes/snow-6.js
+++ _includes/snow-6.js
@@ -1,47 +1,47 @@
 function assignToPlait(prefix, blobs){
-    return blobs.map((item, i) => `${prefix}${i}=${item}`).join(",")
+    return blobs.map((item, i) => prefix.replace(/=/g,i+'=') + item).join(",")
+}
+function spiderTips(prefix, blobs){
+    // TODO only if trailing and leading twist, more complicated for square
+    const last = blobs[blobs.length-1]
+    return ',' + prefix.replace(/=/g,`${blobs.length-1}${last.length-1}=`) + "ctcttctc"
 }
 function no_bounce_update(){
     const blobs = document.getElementById('no-bounce-blobs').value.toLowerCase().split(",");
     const nrOfBlobs = blobs.length;
     const right = document.getElementById("no_bounce_first_blobRight").checked
     const plait = (right ? "rc": "lc").repeat(nrOfBlobs).substring(0,nrOfBlobs)
-    const spiderTip = `${nrOfBlobs-1}${blobs[nrOfBlobs-1].length-1}=ctcttctc`
     const width = document.getElementById("no-bounce-patch-width").value
     const height = document.getElementById("no-bounce-patch-height").value
     const patternType = document.querySelector('input[name="no-bounce-pattern"]:checked').value
     const footsideType = document.querySelector('input[name="no-bounce-footside"]:checked').value
     // headside columns:
-    const h1 = 'abcdefghijklmnopqrstuvwxyz'[width*1+1]
-    const h2 = 'abcdefghijklmnopqrstuvwxyz'[width*1+2]
+    const g = 'abcdefghijklmnopqrstuvwxyz'[width*1+2]
+    const h = 'abcdefghijklmnopqrstuvwxyz'[width*1+3]
     let q = "?"
     if (patternType == "diamond") {
-        q += `c1=${plait}`
+        q += `b1=${plait}&d1=${plait}&${g}2=${plait}`
         q += "&tile=-5&shiftColsSW=-1&shiftRowsSW=1&shiftColsSE=1&shiftRowsSE=1"
-        droste2 = `&droste2=${assignToPlait('c1', blobs)}`
-        droste3 = `&droste3=ctc,c1${spiderTip}`
+        droste2 = `&droste2=${assignToPlait(`b1=d1=${g}2=`, blobs)}`
+        droste3 = '&droste3=ctc' + spiderTips('b1=d1=${g}2=', blobs)
     } else {
         const flippedPlait = plait.replace(/r/g, "L").replace(/l/g, "R").toLowerCase()
         const flippedBlobs = blobs.map(str => str.replace(/r/g, "L") .replace(/l/g, "R") .toLowerCase())
-        q += `b1=${plait}&b2=${flippedPlait}`
+        q += `b1=${plait}&b2=${flippedPlait}&c1=${plait}&c2=${flippedPlait}&${g}1=${plait}&${g}2=${flippedPlait}`
         q += "&tile=8,1&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=1&shiftRowsSE=2"
-        droste2 = `&droste2=${assignToPlait('b1', blobs)},${assignToPlait('b2',flippedBlobs)}`
-        droste3 = `&droste3=ctc,b1${spiderTip},b2${spiderTip}`
+        droste2 = `&droste2=${assignToPlait(`b1=c1=${g}1=`, blobs)},${assignToPlait(`b2=c2=${g}2=`,flippedBlobs)}`
+        droste3 = `&droste3=ctc` + spiderTips(`b1=b2=c1=c2=${g}1==${g}2=`, blobs)
     }
-    q += "&footside=b,-&headside=-,c"
+    q += "&footside=-5,b-&headside=-c,5-"
     switch (footsideType) {
         case "nothing":
-            q += `&a1=-&${h1}2=-`
-            break
-        case "twist":
-            q += `&a1=ttt&${h1}2=ttt`
-            droste2 += `,a10=a11=${h1}20=tttt`
-            droste3 += `,a110=${h1}200=${h1}201=ttttttt`
+            q += `&a2=-&${h}1=-`
             break
         default:
-            q += `&a1=rct&${h1}2=lct`
-            droste2 += `,${h1}22=${blobs[0]},,a13=crrr,a10=a11=a12=${h1}11=ttt,${h1}20=rrr,${h1}12=ttttc,${h1}10=ttttcllll,${h1}21=${h1}23=llctt,${h1}23=tttt`
-            droste3 += `,${h1}213=ctcttctc,,a110=a120=cr,a100=a111=a107=a108=a130=ttttct,a131=ttctctt,c134=c124=a100=ttttttttttt,${h1}212=llctt,${h1}200=tt,${h1}214=ttctt`
+            q += `&a2=ttt&${h}1=ttt`
+            droste2 += `,,a21=${h}10=${g}20=${g}22=tt,${g}12=${g}21=cr,${g}23=r`
+            droste3 += `,,b133=ctctttttt,${g}201=${g}210=${g}211=ttttt`
+            break
     }
     q += `&patchWidth=${width}&patchHeight=${height}`
     document.getElementById("hex_no_bounce_link").href = "https://d-bl.github.io/GroundForge/droste" + q + droste2 + droste3
